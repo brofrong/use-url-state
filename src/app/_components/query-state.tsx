b1b61142173f5e8b2c60cd9099b16a434382262b
dga <dima737.99@mail.ru>
2024-11-20 13:22:40 +0300

    fix parsing

--- src/app/_components/query-state.tsx
+++ src/app/_components/query-state.tsx
@@ -1,15 +1,15 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { useQueryState } from "nuqs";
+import { parseAsInteger, useQueryState } from "nuqs";
 
 const QueryState: React.FC<{ queryParamName: string }> = ({ queryParamName }) => {
-  const [counter, setCounter] = useQueryState(queryParamName, { defaultValue: '0' });
+  const [counter, setCounter] = useQueryState(queryParamName, parseAsInteger.withDefault(0));
 
   return (
     <div>
       <div className="mb-2">{queryParamName}: {counter}</div>
-      <Button onClick={() => setCounter((parseInt(counter) + 1).toString())}>{queryParamName} ++</Button>
+      <Button onClick={() => setCounter(counter + 1)}>{queryParamName} ++</Button>
     </div>
   );
 }
