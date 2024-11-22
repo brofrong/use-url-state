287f058258af887e126c423a3c85caaf12c1bf99
dga <dima737.99@mail.ru>
2024-11-22 12:06:21 +0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
 "use client";
 
+import { Suspense } from "react";
 import QueryStates from "./_components/query-sates";
 import QueryState from "./_components/query-state";
 
 export default function Home() {
   return (
-    <div className="flex flex-col m-auto items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <QueryState queryParamName="counter" />
-      <QueryState queryParamName="counter" />
-      <QueryState queryParamName="test" />
-      <QueryStates />
-    </div>
+    <Suspense>
+      <div className="flex flex-col m-auto items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <QueryState queryParamName="counter" />
+        <QueryState queryParamName="counter" />
+        <QueryState queryParamName="test" />
+        <QueryStates />
+      </div>
+    </Suspense>
   );
 }
